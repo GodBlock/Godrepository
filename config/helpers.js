04cc834be4751007eef77eb05f7389c2a6fe01b4
GodBlock <35083106+GodBlock@users.noreply.github.com>
2021-04-09 15:34:47 -0400

    Update helpers.js

--- config/helpers.js
+++ config/helpers.js
@@ -3,9 +3,9 @@ let MySqli = require('mysqli');
 let conn = new MySqli({
     host: 'localhost',
     port: '3306',
-    user: 'mtri4787_admin',
-    passwd: '[k]I(tQ}FvTA',
-    db: 'balance'
+    user: ''********',',
+    passwd: ''********',',
+    db: ''********','
 });
 
 let db = conn.emit(false, '');
